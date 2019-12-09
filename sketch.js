d7c7e5bb16cc9b6663fd63c89eb056a929b55697
postcarboncollective <58692233+postcarboncollective@users.noreply.github.com>
2019-12-09 13:45:10 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  fill(255);
+  text("Post-Carbon Collective",0,10);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
