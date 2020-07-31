d9460e75978cbb8c2308440d190f4d39d792d157
postcarboncollective <58692233+postcarboncollective@users.noreply.github.com>
2020-07-31 18:59:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,19 +9,17 @@ let cWidth = 0;
 let cHeight = 0;
 let sizeDiv = 54;
 let fCount = 0;
-let eSpeedDiv = 25;
+let eSpeedDiv = 20;
 let seg = 0;
 var xI,xF,yI,yF,xDistance,xGran,yDistance,yGran;
-
-let nPosX = [];
-let nPosY = [];
-nPosX[0] = [];
-nPosY[0] = [];
-
+let hoover=false;
 let nPos = [];
-nPos[0] = [];
-nPos[0][0] = [];
-nPos[0][1] = [];
+for(i=0;i<100;i++){
+  nPos[i] = [];
+  for(j=0;j<2;j++){
+    nPos[i][j]=[];
+  }
+}
 
 function preload() {
   logo = loadImage('assets/PCCLogoBlack.jpg');
@@ -42,11 +40,13 @@ function draw(){
 
 function resetBG(){
   var canvas = createCanvas(windowWidth, windowHeight*0.9);
+  // var canvas = createCanvas(windowHeight*0.9, windowHeight*0.9);
   canvas.parent('sketch-div');
+  // canvas.position(0, 0);
   background(0);
+
   cWidth = width/sizeDiv;
   cHeight = height/sizeDiv;
-
 
   nPos[0][0][0]= width/1.7125;
   nPos[0][1][0]= height/2.56;
@@ -54,6 +54,20 @@ function resetBG(){
   nPos[0][1][1]= height/2.27;
   nPos[0][0][2]= width/1.88;
   nPos[0][1][2]= height/2.05;
+
+  nPos[1][0][0]= width/1.7225;
+  nPos[1][1][0]= height/2.81;
+  nPos[1][0][1]= width/1.84;
+  nPos[1][1][1]= height/3.17;
+  nPos[1][0][2]= width/3.1;
+  nPos[1][1][2]= height/3.18;
+
+  nPos[2][0][0]= width/1.807;
+  nPos[2][1][0]= height/2.72;
+  nPos[2][0][1]= width/2.2;
+  nPos[2][1][1]= height/2.72;
+  nPos[2][0][2]= width/2.395;
+  nPos[2][1][2]= height/2.48;
 }
 
 function drawMenu(){
@@ -64,20 +78,22 @@ function drawMenu(){
     rect(width/1.66,height/2.92,width/16,height/16);
 
     ellipse(nPos[0][0][0],nPos[0][1][0],cWidth,cHeight);
-    ellipse(width/1.7225,height/2.81,cWidth,cHeight);
-    ellipse(width/1.807,height/2.72,cWidth,cHeight);
+    line(nPos[0][0][0],nPos[0][1][0],nPos[0][0][1],nPos[0][1][1]);
+    line(nPos[0][0][1],nPos[0][1][1],nPos[0][0][2],nPos[0][1][2]);
+    ellipse(nPos[1][0][0],nPos[1][1][0],cWidth,cHeight);
+    line(nPos[1][0][0],nPos[1][1][0],nPos[1][0][1],nPos[1][1][1]);
+    line(nPos[1][0][1],nPos[1][1][1],nPos[1][0][2],nPos[1][1][2]);
+    ellipse(nPos[2][0][0],nPos[2][1][0],cWidth,cHeight);
+    line(nPos[2][0][0],nPos[2][1][0],nPos[2][0][1],nPos[2][1][1]);
+    line(nPos[2][0][1],nPos[2][1][1],nPos[2][0][2],nPos[2][1][2]);
+
     ellipse(width/1.555,height/2.115,cWidth,cHeight);
+    line(width/1.555,height/2.115,width/1.42,height/2.115);
+    line(width/1.42,height/2.115,width/1.225,height/1.7);
+
     ellipse(width/1.46,height/2.275,cWidth,cHeight);
     ellipse(width/1.708,height/2,cWidth,cHeight);
 
-    line(nPos[0][0][0],nPos[0][1][0],nPos[0][0][1],nPos[0][1][1]);
-    line(nPos[0][0][1],nPos[0][1][1],nPos[0][0][2],nPos[0][1][2]);
-    line(width/1.7225,height/2.81,width/1.84,height/3.17);
-    line(width/1.84,height/3.17,width/3.1,height/3.18);
-    line(width/1.807,height/2.72,width/2.2,height/2.72);
-    line(width/2.2,height/2.72,width/2.395,height/2.48);
-    line(width/1.555,height/2.115,width/1.42,height/2.115);
-    line(width/1.42,height/2.115,width/1.225,height/1.7);
     line(width/1.46,height/2.275,width/1.29,height/2.275);
     line(width/1.29,height/2.275,width/1.18,height/2.75);
     line(width/1.708,height/2,width/1.708,height/1.45);
@@ -86,8 +102,8 @@ function drawMenu(){
 
     fill(0);
     ellipse(nPos[0][0][2],nPos[0][1][2],cWidth,cHeight);
-    ellipse(width/3.1,height/3.18,cWidth,cHeight);
-    ellipse(width/2.395,height/2.48,cWidth,cHeight);
+    ellipse(nPos[1][0][2],nPos[1][1][2],cWidth,cHeight);
+    ellipse(nPos[2][0][2],nPos[2][1][2],cWidth,cHeight);
     ellipse(width/1.225,height/1.7,cWidth,cHeight);
     ellipse(width/1.18,height/2.75,cWidth,cHeight);
     ellipse(width/1.33,height/1.315,cWidth,cHeight);
@@ -96,47 +112,49 @@ function drawMenu(){
     strokeWeight(0);
     textAlign(RIGHT,CENTER);
     textSize(((width+height)/2)/sizeDiv);
-    text('About', width/3.25, height/3.18);
-    text('Artists', width/2.5, height/2.48);
-    text('Events', width/1.95, height/2.05);
+    text('Events', nPos[0][0][2]-(width/sizeDiv), nPos[0][1][2]);
+    text('About', nPos[1][0][2]-(width/sizeDiv), nPos[1][1][2]);
+    text('Artists', nPos[2][0][2]-(width/sizeDiv), nPos[2][1][2]);
     textAlign(LEFT,CENTER);
     text('Projects', width/1.2, height/1.7);
     text('Label', width/1.155, height/2.75);
     text('Software', width/1.305, height/1.315);
 
-
-    xI = nPos[0][0][nPos[0][0].length-(seg+1)];
-    yI = nPos[0][1][nPos[0][1].length-(seg+1)];
-    xF = nPos[0][0][nPos[0][0].length-(seg+2)];
-    yF = nPos[0][1][nPos[0][1].length-(seg+2)];
-    xDistance = xF-xI;
-    xGran = xDistance/eSpeedDiv;
-    yDistance = yF-yI;
-    yGran = yDistance/eSpeedDiv;
-
-    if(mouseX > (nPos[0][0][nPos[0][0].length-1])-(cWidth) && mouseX < ((nPos[0][0][nPos[0][0].length-1])+(cWidth))){
-      if(mouseY > (nPos[0][1][nPos[0][1].length-1])-(cHeight) && mouseY < (nPos[0][1][nPos[0][1].length-1])+(cHeight)){
-        fill(255);
-        ellipse(nPos[0][0][nPos[0][0].length-1],nPos[0][1][nPos[0][1].length-1],cWidth,cHeight);
-        fill(255,random(100,200));
-        ellipse(xI+ePosX,yI+ePosY,cWidth,cHeight);
-        if(fCount < eSpeedDiv){
-          ePosX += xGran;
-          ePosY += yGran;
-          fCount++;
-        }
-        else{
-          ePosX = 0;
-          ePosY = 0;
-          fCount = 0;
-          seg+=1;
-          if(nPos[0][0].length-(seg+2)<0){
-            seg=0;
+    hoover=false;
+    for(n=0;n<3;n++){
+      xI = nPos[n][0][nPos[n][0].length-(seg+1)];
+      yI = nPos[n][1][nPos[n][1].length-(seg+1)];
+      xF = nPos[n][0][nPos[n][0].length-(seg+2)];
+      yF = nPos[n][1][nPos[n][1].length-(seg+2)];
+      xDistance = xF-xI;
+      xGran = xDistance/eSpeedDiv;
+      yDistance = yF-yI;
+      yGran = yDistance/eSpeedDiv;
+      if(mouseX > (nPos[n][0][nPos[n][0].length-1])-(cWidth) && mouseX < ((nPos[n][0][nPos[n][0].length-1])+(cWidth))){
+        if(mouseY > (nPos[n][1][nPos[n][1].length-1])-(cHeight) && mouseY < (nPos[n][1][nPos[n][1].length-1])+(cHeight)){
+          hoover=true;
+          fill(255);
+          ellipse(nPos[n][0][nPos[n][0].length-1],nPos[n][1][nPos[n][1].length-1],cWidth,cHeight);
+          fill(255,random(100,200));
+          ellipse(xI+ePosX,yI+ePosY,cWidth,cHeight);
+          if(fCount < eSpeedDiv){
+            ePosX += xGran;
+            ePosY += yGran;
+            fCount++;
+          }
+          else{
+            ePosX = 0;
+            ePosY = 0;
+            fCount = 0;
+            seg+=1;
+            if(nPos[0][0].length-(seg+2)<0){
+              seg=0;
+            }
           }
         }
       }
     }
-    else{
+    if(hoover==false){
       ePosX = 0;
       ePosY = 0;
       fCount = 0;
