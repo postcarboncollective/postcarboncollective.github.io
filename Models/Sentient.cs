969b9b7d061f576a0c4be9f628ac6536cffae321
esquilofreniko <esquilofreniko@gmail.com>
2022-06-24 13:51:42 +0100

    Updates

--- Models/Sentient.cs
+++ Models/Sentient.cs
@@ -5,6 +5,7 @@ namespace Postcarbon.Models
     public class Sentient
     {
         public string Name { get; set; } = "Placeholder Name";
+        public string Id { get; set; } = "assets/img/PostcarbonLogoWhite.png";
         public string Picture { get; set; } = "assets/img/PostcarbonLogoWhite.png";
         public string Bio { get; set; } = "Placeholder Biography";
         public List<string> Links { get; set; } = new List<string>();
